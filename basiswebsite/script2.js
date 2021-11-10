e6a5ebc8150f55485c1245e2d4a8084b14e4f71d
merelwiersmaa <83574654+merelwiersmaa@users.noreply.github.com>
2021-11-10 14:27:04 +0100

    Add files via upload

---
+++ basiswebsite/script2.js
@@ -0,0 +1,8 @@
+var zijkant = document.querySelector(".zijkant");
+
+var voorkantfoto = document.querySelector(".voorkantfoto");
+
+function zijkantfoto() {
+    voorkantfoto.src = "watch_belowheader/WATCH2.png"
+    }
+    zijkant.addEventListener("click", zijkantfoto);
